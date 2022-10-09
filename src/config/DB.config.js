1a31f0499ef89ebd3f2c627c669591a403fc6c83
saurabhrai2834 <33933938+saurabh688@users.noreply.github.com>
2022-10-09 13:40:16 -0400

    Update DB.config.js

--- src/config/DB.config.js
+++ src/config/DB.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    HOST_LOCAL: "107.21.67.243",
+    HOST_LOCAL: "3.91.158.108",
     USER_LOCAL: "trox",
     PASSWORD_LOCAL: "Password_123_!",
     DB_LOCAL: "trox",
 
-    HOST_STAGE: "107.21.67.243",
+    HOST_STAGE: "3.91.158.108",
     USER_STAGE: "trox",
     PASSWORD_STAGE: "Password_123_!",
     DB_STAGE: "trox",
