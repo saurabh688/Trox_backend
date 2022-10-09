7b238d3951e0c569bb39617fa76b712dc69f842a
Nihar <sahunihar0602@gmail.com>
2022-10-09 01:42:52 -0400

    Changed port

--- src/config/constants/Constants.js
+++ src/config/constants/Constants.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 module.exports = {
-    PORT: process.env.PORT || 8081,
+    PORT: process.env.PORT || 4000,
     API_VERSION: "/api/v1",
     SALT: "$2a$20$/SKvBPuijjf3kB1i7S7fbO"
 }
